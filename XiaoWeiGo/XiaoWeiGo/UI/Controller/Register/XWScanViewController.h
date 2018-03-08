fdeb3b2cf627c95ee311172eff42e64dcb7da48b
ziyao <dingxin@iMac-2.local>
2018-03-08 21:52:40 +0800

    添加文件

---
+++ XiaoWeiGo/XiaoWeiGo/UI/Controller/Register/XWScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  XWScanViewController.h
+//  XiaoWeiGo
+//
+//  Created by dingxin on 2018/3/8.
+//  Copyright © 2018年 xwjy. All rights reserved.
+//
+
+#import "XWBaseViewController.h"
+
+@interface XWScanViewController : XWBaseViewController
+
+@end
